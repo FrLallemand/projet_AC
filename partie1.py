574120d455580ca174c8b61e9c3ee52368cd9cd5
François Lallemand <frlallemand@protonmail.com>
2017-12-03 17:22:32 +0100

    deux méthodes proposées, la recursive peut être plus simple à prouver en O(log(n))

--- partie1.py
+++ partie1.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import sys
-sys.setrecursionlimit(10000) # 10000 is an example, try with different values
 
 '''
 Calcule le produit de deux nombres x et y necessitant plus de 32 bits
@@ -34,4 +32,18 @@ def puissance(x, k, p):
         x = multiply(x, x , p)
     return result
 
-print(puissance(125, 687, 15))
+'''
+Calcule x^k mod p de façon récursive
+'''
+def puissance_recursion(x, k, p):
+    result = x % p;
+    if k == 0:
+        return 1
+    elif k == 1:
+        return result
+    elif k%2 == 0:
+        return puissance_recursion(multiply(result, result, p), k//2, p)
+    else :
+        return multiply(result, puissance_recursion(result, k-1, p), p)
+
+print(puissance_recursion(125, 687, 15))
